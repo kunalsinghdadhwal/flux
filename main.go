048c8bc0d99af65ea5310ebac8e65c7bcbe1d8cc
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-12-22 22:23:50 +0530

    Line wise print
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello golang and http")
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal("Error", err)
+	}
+
+	str := ""
+
+	for {
+		data := make([]byte, 8)
+		n, err := f.Read(data)
+		if err != nil {
+			break
+		}
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			fmt.Printf("Read: %s\n", str)
+			str = string(data[i+1:])
+		} else {
+			str += string(data)
+		}
+	}
+	if len(str) != 0 {
+		fmt.Printf("Read: %s\n", str)
+	}
 }
