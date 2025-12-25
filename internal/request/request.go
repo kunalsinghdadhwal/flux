7a52fd6197b235ca1763b8115e84ed11ca28328c
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-12-26 00:17:47 +0530

    request parser change
    Signed-off-by: Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 )
@@ -11,12 +12,105 @@ type RequestLine struct {
 	Method        string
 }
 
+type parserState string
+
+const (
+	StateInit parserState = "init"
+	StateDone parserState = "done"
+)
+
 type Request struct {
 	RequestLine RequestLine
+	state       parserState
 }
 
-var ERRO_BAD_START_LINE = fmt.Errorf("BAD START LINE IN THE REQUEST") 
+func newRequest() *Request {
+	return &Request{
+		state: StateInit,
+	}
+}
+
+var ERROR_MALFORMED_REQUEST_LINE = fmt.Errorf("MALFORMED LINE IN THE REQUEST")
+var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("HTTP VERSION NOT SUPPORTED, ONLY SUPPORTS 1.1")
+var SEPERATOR = []byte("\r\n")
+
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(b, SEPERATOR)
+	if idx == -1 {
+		return nil, 0, nil
+	}
+
+	startLine := b[:idx]
+	read := idx + len(SEPERATOR)
+
+	parts := bytes.Split(startLine, []byte(" "))
+	if len(parts) != 3 {
+		return nil, 0, ERROR_MALFORMED_REQUEST_LINE
+	}
+
+	httpParts := bytes.Split(parts[2], []byte("/"))
+	if len(httpParts) != 2 || string(httpParts[0]) != "HTTP" || string(httpParts[1]) != "1.1" {
+		return nil, 0, ERROR_MALFORMED_REQUEST_LINE
+	}
+
+	rl := &RequestLine{
+		Method:        string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HttpVersion:   string(httpParts[1]),
+	}
+
+	return rl, read, nil
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	read := 0
+outer:
+	for {
+		switch r.state {
+		case StateInit:
+			rl, n, err := parseRequestLine(data[read:])
+			if err != nil {
+				return 0, err
+			}
+
+			if n == 0 {
+				break outer
+			}
+			r.RequestLine = *rl
+			read += n
+
+			r.state = StateDone
+		case StateDone:
+			break outer
+		}
+	}
+	return read, nil
+}
+
+func (r *Request) done() bool {
+	return r.state == StateDone
+}
 
 func RequestFromReader(r io.Reader) (*Request, error) {
-	
+	request := newRequest()
+
+	buf := make([]byte, 1024)
+	bufLen := 0
+	for !request.done() {
+		n, err := r.Read(buf[bufLen:])
+		if err != nil {
+			return nil, err
+		}
+
+		readN, err := request.parse(buf[:bufLen])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[readN:bufLen])
+		bufLen -= readN
+
+	}
+
+	return request, nil
 }
